2a65f4d71bb1da0829b65dc6cf740ad9f4c5a9ca
kyanag <gongzuo_yk@foxmail.com>
2020-07-09 18:25:45 +0800

    1

--- resources/views/image/worktable.blade.php
+++ resources/views/image/worktable.blade.php
@@ -49,49 +49,34 @@
 <div class="container">
     <div class="card-deck row justify-content-md-center text-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card ">
                 <div class="card-header">
                     <h4 class="my-0 font-weight-normal">证件照换背景色</h4>
                 </div>
-                <div class="card-body">
-                    <button type="button" class="btn btn-primary btn-sm" id="file-select-btn"><i class="fa fa-upload" aria-hidden="true"></i> 选择图片</button>
-                    <div class="pricing-header px-3 py-1 mx-auto text-center">
-                        <p class="lead">任意一张带有正脸人物头像的照片，即可轻松处理为证件照</p>
+                <div class="card-body bg-light">
+                    <div class="pricing-header px-3 mx-auto text-center">
+                        <p class="lead">1. 选择你的证件照</p>
+                        <p class="lead">2. 选择颜色</p>
+                        <p class="lead">3. 下载照片</p>
                     </div>
+                    <button type="button" class="btn btn-primary btn-sm pm-1" id="file-select-btn"><i class="fa fa-upload" aria-hidden="true"></i> 选择图片</button>
                     <div class="row">
                         <div class="col-md-12">
-                            <div id="image-face" class="d-flex mx-auto align-items-end" style="width: 250px;height: 350px;background-color: #dee2e6">
-                                <img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22250%22%20height%3D%22350%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20250%20350%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158bd1d28ef%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158bd1d28ef%22%3E%3Crect%20width%3D%22250%22%20height%3D%22350%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%0A%3Ctext%20x%3D%2250%25%22%20y%3D%2245%25%22%20dy%3D%22.3em%22%20text-anchor%3D%22middle%22%20font-family%3D%22MSYH%22%3E%E7%82%B9%E5%87%BB%20%5B%E9%80%89%E6%8B%A9%E5%9B%BE%E7%89%87%5D%3C%2Ftext%3E%0A%3Ctext%20x%3D%2250%25%22%20y%3D%2255%25%22%20dy%3D%22.3em%22%20text-anchor%3D%22middle%22%20font-family%3D%22MSYH%22%3E%20%E4%B8%8A%E4%BC%A0%E4%BB%BB%E6%84%8F%E6%AD%A3%E9%9D%A2%E5%9B%BE%3C%2Ftext%3E%0A%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" class="img-fluid mx-auto" style="max-width: 100%; height: auto;" id="image-face-img"></img>
+                            <div id="image-face" class="d-flex mx-auto align-items-end pt-3">
+                                <canvas id="image-canvas" class="m-auto"></canvas>
                             </div>
-
-{{--                            <svg width="250" height="350" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 350">--}}
-{{--                                <defs><style type="text/css">#holder_158bd1d28ef text { fill:rgba(255,255,255,.75);font-weight:normal;font-family:Helvetica, monospace;font-size:16pt } </style></defs>--}}
-{{--                                <g id="holder_158bd1d28ef">--}}
-{{--                                    <rect width="250" height="350" fill="#777"></rect>--}}
-{{--                                    <g>--}}
-{{--                                        <text x="50%" y="45%" dy=".3em" text-anchor="middle" font-family="MSYH">点击 [选择图片]</text>--}}
-{{--                                        <text x="50%" y="55%" dy=".3em" text-anchor="middle" font-family="MSYH"> 上传任意正面图</text>--}}
-{{--                                    </g>--}}
-{{--                                </g>--}}
-{{--                            </svg>--}}
                         </div>
                         <div class="col-md-12 m-0 pt-3">
-                            <div class="btn-group btn-group-sm pt-1" aria-label="Basic example">
+                            <div class="btn-group btn-group-sm pt-1" aria-label="背景色" id="actionbar-color">
                                 <button type="button" class="btn btn-default ml-1">背景色</button>
-                                <button type="button" class="btn btn-outline-primary matting-color" data-color="#FFFFFF">白</button>
-                                <button type="button" class="btn btn-outline-danger matting-color" data-color="#FF0000">红色</button>
-                                <button type="button" class="btn btn-primary matting-color"  data-color="#00BFF3">蓝色</button>
                             </div>
-                            <div class="btn-group btn-group-sm pt-1 ml-2">
-                                <button type="button" class="btn btn-default">尺寸</button>
-                                <button type="button" class="btn btn-outline-info" data-size="250*350">1寸</button>
-                                <button type="button" class="btn btn-outline-info" data-size="350*490">2寸</button>
-                                <button type="button" class="btn btn-outline-info" data-size="350*520">3寸</button>
-                                <button type="button" class="btn btn-outline-info ml-1" data-size="250*350">港澳台</button>
+
+                            <div class="btn-group btn-group-sm pt-1 ml-2" aria-label="裁剪" id="actionbar-size" style="display: none">
+                                <button type="button" class="btn btn-default btn-sm">裁剪</button>
                             </div>
                         </div>
                     </div>
-                    <button type="button" class="btn btn-sm btn-block btn-primary mt-4">下载</button>
+                    <button type="button" class="btn btn-sm btn-block btn-primary mt-4" id="download">下载</button>
                     <button type="button" class="btn btn-sm btn-block btn-warning mt-2">赞赏</button>
                     <div class="btn-group btn-group-sm mt-2">
                         <button type="button" class="btn btn-sm btn-success mt-2">微信小程序</button>
@@ -140,33 +125,143 @@
 </div>
 <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 <script src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<script src="https://cdn.bootcdn.net/ajax/libs/webuploader/0.1.1/webuploader.js"></script>
+<script src="https://cdn.bootcdn.net/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.js"></script>
+
 <script>
     var HeadMatting = function(options){
-        this.COLOR_RED = "#FF0000";
-        this.COLOR_WHITE = "#FFFFFF";
-        this.COLOR_BLUE = "#00BFF3";
+        const COLOR_RED = {rgb:"#FF0000", name: "红"};
+        const COLOR_WHITE = {rgb:"#FFFFFF", name: "白"};;
+        const COLOR_BLUE = {rgb:"#00BFF3", name: "蓝"};;
 
-        this.SIZE_1 = {width:250, height:350};
-        this.SIZE_2 = {width: 350, height: 490};
-        this.SIZE_3 = {width:350,height: 520};
-        this.SIZE_G_A_T = {width:250, height: 350};
+        const SIZE_1         = {width: 295, height: 413, name: "1寸"};
+        const SIZE_2_LITTLE  = {width: 390, height: 567, name: "小2寸"};
+        const SIZE_2         = {width: 413, height: 626, name: "2寸"};
+        //const SIZE_SFZ       = {width: 260, height: 390, name: "身份证"};
 
-
+        var activeColor = COLOR_RED;
+        var activeSize = SIZE_1;
         var imageBlob;
+        var canvasCtx;
 
-        this.setImage = function(image){
-            imageBlob = image;
+        this.setImage = function(img){
+            imageBlob = img;
+            this.drawImage();
+        };
 
-            var reader = new window.FileReader();
-            reader.readAsDataURL(image);
-            reader.onloadend = function() {
-                $("#image-face-img").attr("src", reader.result);
-            };
+        this.mockImage = function(){
+            var svg = `
+<svg width="${activeSize.width}" height="${activeSize.height}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${activeSize.width} ${activeSize.height}" preserveAspectRatio="none">
+    <defs>
+        <style type="text/css">#holder_158bd1d28ef text { fill:rgba(255,255,255,.75);font-weight:normal;font-family:Helvetica} </style>
+    </defs>
+    <g id="holder_158bd1d28ef"><rect width="${activeSize.width}" height="${activeSize.height}" fill="#777"></rect>
+        <g>
+        <text x="50%" y="45%" dy=".3em" text-anchor="middle" font-family="MSYH">点击 [选择图片]</text>
+        <text x="50%" y="55%" dy=".3em" text-anchor="middle" font-family="MSYH"> 上传任意正面图</text>
+        </g>
+    </g>
+</svg>`;
+            var img = new Image();
+            img.src = 'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(svg)));//svg内容中可以有中文字符
+            return img;
         };
 
         this.setColor = function (color) {
-            $("#image-face").css("background-color", color);
+            activeColor = {rgb:color};
+            this.drawImage();
+        };
+
+        this.init = function(){
+            canvasCtx = document.querySelector("#image-canvas").getContext("2d");
+
+            var sizes = [
+                SIZE_1,
+                SIZE_2_LITTLE,
+                SIZE_2,
+            ];
+            var colors = [
+                COLOR_WHITE,
+                COLOR_RED,
+                COLOR_BLUE,
+            ];
+            sizes.forEach( size => {
+                var sizeBtn = `<button type="button" class="btn btn-outline-info matting-size" data-size="${size.width}*${size.height}">${size.name}</button>`
+                $("#actionbar-size").append(sizeBtn);
+            });
+
+            colors.forEach( color => {
+                var colorBtn = `<button type="button" class="btn btn-outline-primary matting-color" data-color="${color.rgb}">${color.name}</button>`;
+
+                $("#actionbar-color").append(colorBtn);
+            });
+            this.registerEvents();
+
+            //this.setSize(SIZE_1);
+            this.drawImage();
+        };
+
+        this.registerEvents = function(){
+            $("#file-select-btn").click(() => {
+                this.showOpenDialog().then((file) => {
+                    return this.uploadFile(file);
+                }).then((response) => {
+                    var blob = new Blob([response]);
+
+                    this.setImage(blob);
+                });
+            });
+
+            $(".matting-color").click((e) => {
+                this.setColor($(e.target).data("color"));
+            });
+            $(".matting-size").click((e) => {
+                var size = $(e.target).data("size");
+                var [width, height] = size.split("*");
+
+                this.setSize({width, height});
+
+                $(".matting-size").each(function(){
+                    $(this).removeClass("btn-info");
+                    $(this).addClass("btn-outline-info");
+                });
+
+                $(this).addClass("btn-info");
+                $(this).removeClass("btn-outline-info");
+            });
+
+            $("#download").click(() => {
+                this.download();
+            })
+        };
+
+        this.drawImage = function(){
+            var image;
+            if(imageBlob){
+                image = new Image();
+                image.src = URL.createObjectURL(imageBlob);
+            }else{
+                image = this.mockImage();
+            }
+            image.onload = () => {
+                //清空画布
+                $("#image-canvas")
+                    .attr("width", image.width)
+                    .attr("height", image.height);
+
+                canvasCtx.fillStyle = activeColor.rgb;
+                canvasCtx.fillRect(0, 0, image.width, image.height);
+
+                canvasCtx.drawImage(image, 0, 0);
+            };
+            document.body.appendChild(image);
+        };
+
+        this.setSize = function (size) {
+            console.log(size);
+            activeSize = size;
+            $("#image-face")
+                .css("width", `${size.width}px`)
+                .css("height", `${size.height}px`);
         };
 
         this.showOpenDialog = function(){
@@ -210,55 +305,29 @@
                 };
                 var fd = new FormData();
                 fd.append("file", file);
-                fd.append("_token", "{{ csrf_token() }}");
+                fd.append("_token", "<?php echo e(csrf_token()); ?>");
                 xhr.send(fd);
             });
+        };
+
+        this.download = function(){
+            var canvas = document.querySelector("#image-canvas");
+            var url = canvas.toDataURL("image/jpeg");
+
+            var link = document.createElement("a");
+            link.style.display = "none";
+            link.href = url;
+            link.setAttribute("download", "证件照.jpg");
+            document.body.appendChild(link);
+            link.click();
+
+            setTimeout(link.remove, 1000);
         };
     };
 
     $(function () {
         let app = new HeadMatting();
-
-        $("#file-select-btn").click(function(){
-            app.showOpenDialog().then(function(file){
-                return app.uploadFile(file);
-            }).then(function(response){
-                app.setImage(new Blob([response]));
-            });
-        });
-
-        $(".matting-color").click(function(){
-            app.setColor($(this).data("color"));
-        });
-        {{--let uploader = WebUploader.create({--}}
-        {{--    server: "{{ route("image.headmatting") }}",--}}
-        {{--    pick: '#file-select-btn',--}}
-        {{--    resize: false,--}}
-        {{--    chunked: false,--}}
-        {{--    //chunkSize: 1024 * 1024,         //每次分片1M--}}
-        {{--    //fileNumLimit: 1,--}}
-        {{--    auto: true,--}}
-        {{--    duplicate: true,--}}
-        {{--    //fileSizeLimit: 2 * 1024 * 1024,    //最大2M--}}
-        {{--    accept: {--}}
-        {{--        //mimeTypes: 'image/*'--}}
-        {{--    },--}}
-        {{--    threads: 1,--}}
-        {{--    fileVal:"{{ \App\Supports\Uploader::ATTR_VAL_FILE }}",--}}
-        {{--    formData: {--}}
-        {{--        _token:'{{ csrf_token() }}'--}}
-        {{--    }--}}
-        {{--});--}}
-        {{--uploader.on("beforeXhrSend", function () {--}}
-        {{--    console.log(arguments);--}}
-        {{--});--}}
-        {{--uploader.on("uploadSuccess", (file, response) => {--}}
-        {{--    app.setImage(new Blob([response._raw]));--}}
-        {{--});--}}
-        {{--uploader.on("error", (type, file) => {--}}
-        {{--    console.log(type, file);--}}
-        {{--});--}}
-        {{--window.uploader = uploader;--}}
+        app.init();
     });
 </script>
 </body>
